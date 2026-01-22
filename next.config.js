f6b4482c49350e2d954521151bb3d66bb577d1ff
Ease AI / Adam <info@easeaiworks.com>
2026-01-22 11:01:45 -0500

    Simplify next.config.js by removing webpack config
    Removed webpack configuration to exclude supabase functions from build.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Exclude supabase functions from build
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /supabase[\\/]functions/,
-      use: 'null-loader'
-    })
-    return config
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
